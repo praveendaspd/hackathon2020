2b7c6e15ed3a07e62dc223264f92a6c9437ee3a4
PD <praveendaspd@gmail.com>
2020-10-15 09:28:07 +1100

    Code Refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { BannerDetailsComponent } from './components/banner-details/banner-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: TutorialsListComponent },
-  /*{ path: 'tutorials', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent },*/
   { path: 'banner', component: BannerDetailsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import {BannerDetailsComponent} from "./components/banner-details/banner-details.component";
+import {BannerDetailsComponent} from './components/banner-details/banner-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
     TutorialsListComponent,
     BannerDetailsComponent
   ],
